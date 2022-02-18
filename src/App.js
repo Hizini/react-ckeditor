3920c9f65865cf839ffe920ef38c32dae0ab668b
KimHeeJin <ejfdnrl@naver.com>
2022-02-18 13:23:13 +0900

    02-18 ckeditor 기본

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="editor-container">
+      <div className='editor-title'>기본</div>
+      <div className='editor'>
+        <CKEditor
+          editor={ClassicEditor}
+          data="<p>Hello from CKEditor 5!</p>"
+          onReady={editor => {
+            // You can store the "editor" and use when it is needed.
+            console.log('Editor is ready to use!', editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+          }}
+          onBlur={(event, editor) => {
+            console.log('Blur.', editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log('Focus.', editor);
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
