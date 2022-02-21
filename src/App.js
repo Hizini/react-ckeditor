ffdb5c26bee08f3d7fb6578bda53383b3664145a
KimHeeJin <ejfdnrl@naver.com>
2022-02-21 14:11:50 +0900

    데이터 가져오기

--- src/App.js
+++ src/App.js
@@ -3,21 +3,29 @@
 import React, { Component } from 'react'
 import './App.css';
 
+const { ClassicEditor } = window
 class App extends Component {
   constructor(props) {
     super(props)
+
     let editor
   }
   
   async componentDidMount() {
-    const { ClassicEditor } = window
     this.editor = await ClassicEditor.create( document.querySelector( '#editor' ) )
+
+    document.querySelector( '#submit' ).addEventListener( 'click', () => { 
+      const editorData = this.editor.getData() 
+      console.log(this.editor.getData())
+    } )
   }
 
   render() {
     return (
       <div className="editor-container">
         <div className='editor-title'>드디어..</div>
+        <div id='editor'></div>
+        <button id="submit">Submit</button>
       </div>
     ); 
   }
